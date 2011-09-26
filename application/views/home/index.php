3e1c93457ad52301b878cb5ffbbd226d3cdd60d5
codemastersnake <codemaster.snake@gmail.com>
2011-09-26 16:06:55 +0530

    created inputbox

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,4 +1,9 @@
 <?php $this->load->view('header'); ?>
 <h1>Welcome to Project Alice!</h2>
 <p>Project alice is an implementation of naive bayesian classifiers in php.</p>
+
+<div class="inputbox">
+	<input type="text" name="text" id="text" value="Enter text here..." />
+</div>
+
 <?php $this->load->view('footer'); ?>
