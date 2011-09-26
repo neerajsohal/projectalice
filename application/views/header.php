71bfd2a4f3c1027f9cfed9e400d47de2068753d2
codemastersnake <codemaster.snake@gmail.com>
2011-09-26 15:48:34 +0530

    created style.css

--- application/views/header.php
+++ application/views/header.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
 	<title>Project Alice</title>
+	<base href="<?php echo base_url(); ?>" />
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,300,400,700' rel='stylesheet' type='text/css'>
+	<link href='/statics/css/style.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
